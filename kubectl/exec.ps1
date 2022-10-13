398eedfec479b9f9c16a304d0e602208d4b1bdf3
Chris Gutmanis <chris.gutmanis@gmail.com>
2022-10-12 20:22:01 -0500

    Update exec.ps1

--- kubectl/exec.ps1
+++ kubectl/exec.ps1
@@ -1,4 +1,4 @@
-# A lazy way to shell into running containers without having to constantly look up the pod hash.
+# A lazy way to shell into running containers without having to constantly look up the pod hash and save a couple keystrokes. 
 $name=$args[0]
 $namespace=$args[1]
 $pod = kubectl get pods -l io.kompose.service=$name -n $namespace -o jsonpath="{.items[0].metadata.name}"
